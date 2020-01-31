7a0d2e6dec478692edffb4030338fb105351cf08
agohorel <gohorelalex@gmail.com>
2020-01-31 13:09:26 -0500

    tweak potion.js to use Partition layout w/ rects

--- client/src/components/Bubbles.js
+++ client/src/components/Bubbles.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Pack } from "@potion/layout";
-import { Svg, Circle } from "@potion/element";
+import { Partition } from "@potion/layout";
+import { Svg, Rect } from "@potion/element";
 
 const Bubbles = ({ colors }) => {
   const [bubbleData, setBubbleData] = useState([]);
@@ -15,38 +15,37 @@ const Bubbles = ({ colors }) => {
   return (
     <div className="bubble-wrap">
       <Svg width={400} height={400}>
-        <Pack
-          data={{
-            children: bubbleData
-          }}
+        <Partition
+          data={{ children: bubbleData }}
           sum={datum => datum.value}
           size={[400, 400]}
-          includeRoot={false}
           nodeEnter={d => ({ ...d, r: 0 })}
           animate
         >
           {nodes =>
-            nodes
-              .map(({ x, y, r, key }, i) => {
-                if (i < colors.length) {
-                  return (
-                    <Circle
-                      key={key}
-                      cx={x}
-                      cy={y}
-                      r={r}
-                      fill={colors[i].code.hex}
-                    />
-                  );
-                }
-                return null;
-              })
-              .filter(v => v)
+            nodes.map(({ key, x0, y0, x1, y1 }, i) => {
+              if (i < colors.length) {
+                return (
+                  <Rect
+                    key={key}
+                    x={x0}
+                    y={y0}
+                    width={x1 - x0}
+                    height={y1 - y0}
+                    fill={colors[i].code.hex}
+                  />
+                );
+              }
+              return null;
+            })
           }
-        </Pack>
+        </Partition>
       </Svg>
     </div>
   );
 };
 
 export default Bubbles;
+
+{
+}
